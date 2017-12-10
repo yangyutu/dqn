fb9ea3225ce2720252e49015ff51b74c25393532
Brett Daley <brett-daley@users.noreply.github.com>
2017-12-10 03:37:01 -0500

    temp -- use episodes in replay memory

--- dqn/replay_memory.py
+++ dqn/replay_memory.py
@@ -1,10 +1,18 @@
 import numpy as np
 
 
+class Episode:
+    def __init__(self):
+        self.obs = []
+        self.action = []
+        self.reward = []
+        self.next_obs = []
+
+
 class ReplayMemory:
     def __init__(self, config, obs_shape, recurrent_mode, forced_capacity=None):
         self.recurrent_mode = recurrent_mode
-        self.agent_history_length = int(config.get('agent', 'history_length'))
+        #self.agent_history_length = int(config.get('agent', 'history_length'))
         self.batch_size = int(config.get('replay_memory', 'batch_size'))
         self.capacity = forced_capacity if forced_capacity else int(config.get('replay_memory', 'capacity'))
         self.obs_dtype = config.get('replay_memory', 'obs_dtype')
@@ -14,26 +22,29 @@ class ReplayMemory:
         self.ready_for_outcome = False  # Ensures observation's outcome is saved before incrementing the insertion pointer
 
         # Pre-allocate the replay memory; this has better performance and we'll also know immediately if it's too big
-        self.obs = np.zeros([self.capacity] + list(obs_shape), dtype=getattr(np, self.obs_dtype))
-        self.action = np.zeros([self.capacity], dtype=np.int32)
-        self.reward = np.zeros([self.capacity], dtype=np.float32)
-        self.terminal = np.zeros([self.capacity], dtype=np.bool)
+        self.episodes = [Episode() for i in range(self.capacity)]
+        #self.obs = np.zeros([self.capacity] + list(obs_shape), dtype=getattr(np, self.obs_dtype))
+        #self.action = np.zeros([self.capacity], dtype=np.int32)
+        #self.reward = np.zeros([self.capacity], dtype=np.float32)
+        #self.terminal = np.zeros([self.capacity], dtype=np.bool)
 
     def save_obs(self, obs):
         assert not self.ready_for_outcome
         self.ready_for_outcome = True
 
-        self.obs[self.insertion_ptr] = obs
+        self.episodes[self.insertion_ptr].obs = obs
 
     def save_outcome(self, action, reward, terminal):
         assert self.ready_for_outcome
         self.ready_for_outcome = False
 
-        self.action[self.insertion_ptr] = action
-        self.reward[self.insertion_ptr] = reward
-        self.terminal[self.insertion_ptr] = terminal
+        self.episodes[self.insertion_ptr].action = action
+        self.episodes[self.insertion_ptr].reward = reward
+        self.episodes[self.insertion_ptr].terminal = terminal
 
-        self._update_insertion_ptr()
+        if terminal:
+            self._update_insertion_ptr()
+            self.episodes[self.insertion_ptr] = Episode()
 
     def _update_insertion_ptr(self):
         # Increment size unless capacity is reached
@@ -44,13 +55,17 @@ class ReplayMemory:
         self.insertion_ptr = (self.insertion_ptr + 1) % self.capacity
 
     def append_history_to_obs(self, obs):
+        '''
         starting_index = self.insertion_ptr - self.agent_history_length
         offsets = range(self.agent_history_length)
 
         obs = np.concatenate([self.obs[starting_index+j] for j in offsets], axis=-1)
         return obs
+        '''
+        raise NotImplementedError
 
     def sample_minibatch(self):
+        '''
         starting_indices = np.random.choice(self.size - self.agent_history_length, self.batch_size)
         offsets = range(self.agent_history_length)
 
@@ -63,5 +78,13 @@ class ReplayMemory:
         axis = 0 if self.recurrent_mode else -1
         obs_batch = np.concatenate(obs_batch, axis)
         next_obs_batch = np.concatenate(next_obs_batch, axis)
+        '''
+
+        i = np.random.choice(self.size)
+        obs_batch = np.array(self.episodes[i].obs)
+        action_batch = np.array(self.episodes[i].action)
+        reward_batch = np.array(self.episodes[i].reward)
+        next_obs = np.array(self.episodes[i].next_obs)
+        non_terminal_multiplier = 1 - np.array(self.episodes[i].terminal)
 
         return obs_batch, action_batch, reward_batch, next_obs_batch, non_terminal_multiplier
